8600980b93e1c9624fe5c573688e948c40d41fe5
João <joaopauloff7@hotmail.com>
2022-08-15 00:06:53 -0300

    adição do banco de dados

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express")
 const mongoose = require("mongoose")
 
+mongoose.connect('mongodb+srv://joao:KUDqRH6CJsuYRJX@cluster0.3wr9sde.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.Promise = global.Promise
+try {
+    let db = mongoose.connection
+    db.on('errr', console.error.bind(console, 'erro de conexao no banco'))
+} catch (e) {
+    console.log(e)
+}
+
+
 const app = express()
 
 app.use(express.urlencoded({extended: true}));
