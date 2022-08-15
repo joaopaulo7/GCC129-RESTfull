0780c9c793f243e9ebb6871cfad44698765f7250
João <joaopauloff7@hotmail.com>
2022-08-14 23:42:33 -0300

    Início da implementação (teste)

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express()
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send('teste oi')
+})
+
+let porta = process.env.PORT || 3000
+
+app.listen(porta, () => {
+	console.log("servidor em execucao na porta " + porta)
+})
+
