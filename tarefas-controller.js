6992f5e85ea20de14933f3adf90228743d0f12a4
João <joaopauloff7@hotmail.com>
2022-08-16 15:33:51 -0300

    bugfix da criação de tarefa sem descrição

--- tarefas-controller.js
+++ tarefas-controller.js
@@ -22,8 +22,8 @@ exports.buscarTarefa = function (req, res) {
 //Cria um objeto tarefa com os dados dados na requisição e envia pro DB
 exports.cadastrarTarefa = function (req, res) {
     if(req.body.descricao == null){
-        res.send("Descrição vazia")
-        throw("Descrição vazia")
+        res.send("ERRO: Descrição vazia")
+        throw("ERRO: Descrição vazia")
     }
     let tarefa = new tarefas({
         descricao: req.body.descricao,
